824696811a0939b6638c2f8096b6db9ab9980636
Takasaka <tksk4yh@yahoo.co.jp>
2021-10-10 16:32:00 +0900

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/katakasa4444/todo-goapp
+
+go 1.16
+
+require github.com/maxence-charriere/go-app/v9 v9.0.0
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+)
+
+func main() {
+	http.Handle("/", new(hello).Handler("/"))
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type hello struct {
+	app.Compo
+}
+
+func (h *hello) Render() app.UI {
+	return app.H1().Text("Hello World!")
+}
+
+func (h *hello) Handler(path string) http.Handler {
+	app.Route(path, h)
+	app.RunWhenOnBrowser()
+	return &app.Handler{
+		Description: "An Hello World! example",
+		Name:        "Hello",
+	}
+}
